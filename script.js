5b71471d542b37ea355a6af83cb42459fb718b12
Diana <dcampos0495@gmail.com>
2021-12-04 13:46:12 -0600

    se agregan archivos html css y js

---
+++ script.js
@@ -0,0 +1,19 @@
+function comida() {
+window.location.href ="file:///home/diana/Diana/Trainee/pagina_web/comida.html"
+}
+
+function musica() {
+    window.location.href ="file:///home/diana/Diana/Trainee/pagina_web/musica.html"
+    }
+
+function peliculas() {
+window.location.href ="file:///home/diana/Diana/Trainee/pagina_web/peliculas.html"
+}
+
+function lugares() {
+    window.location.href ="file:///home/diana/Diana/Trainee/pagina_web/lugares.html"
+    }
+
+function regreso() {
+window.location.href ="file:///home/diana/Diana/Trainee/pagina_web/index.html"
+}
